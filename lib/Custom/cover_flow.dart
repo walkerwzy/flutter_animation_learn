d9deae73ee2553fea575460f9ecd1318161607ab
walker <walker.wzy@gmail.com>
2018-08-31 20:39:29 +0800

    complete

--- lib/Custom/cover_flow.dart
+++ lib/Custom/cover_flow.dart
@@ -18,7 +18,7 @@ class MyCoverFlowState extends State<MyCoverFlow> {
     controller = new PageController(
       initialPage: currentpage,
       keepPage: false,
-      viewportFraction: 0.8,
+      viewportFraction: 0.4,
     );
   }
 
@@ -50,38 +50,89 @@ class MyCoverFlowState extends State<MyCoverFlow> {
   Widget builder(int index, BoxConstraints constraints) {
     int p = index * 100 + 100;
     p = min(p, 800);
-    Widget item = Container(
-      margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: Colors.green[p],
-          boxShadow: [
-            BoxShadow(
-                color: Colors.grey, blurRadius: 7.0, offset: Offset(2.0, 2.0)),
-          ]),
-      child: Center(
-          child: Text(
-        "page $index",
-      )),
+    // Widget item = Container(
+    //   margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+    //   decoration: BoxDecoration(
+    //       borderRadius: BorderRadius.circular(10.0),
+    //       color: Colors.green[p],
+    //       boxShadow: [
+    //         BoxShadow(
+    //             color: Colors.grey, blurRadius: 7.0, offset: Offset(2.0, 2.0)),
+    //       ]),
+    //   child: Center(
+    //       child: Text(
+    //     "page $index",
+    //   )),
+    // );
+    // Widget buildChild(double fraction) {
+    //   print(fraction);
+    //   return SizedBox(
+    //       width: fraction * (constraints.maxWidth - 80),
+    //       height: fraction * (constraints.maxHeight - 100),
+    //       child: Container(
+    //         // margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+    //         decoration: BoxDecoration(
+    //             borderRadius: BorderRadius.circular(10.0),
+    //             color: Colors.green[p],
+    //             boxShadow: [
+    //               BoxShadow(
+    //                   color: Colors.grey,
+    //                   blurRadius: 7.0,
+    //                   offset: Offset(2.0, 2.0)),
+    //             ]),
+    //         child: Center(
+    //             child: Text(
+    //           "page $index\n${fraction.toStringAsFixed(2)}",
+    //         )),
+    //       ));
+    // }
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        var zoom = 1.0;
+        if (controller.position.haveDimensions) {
+          // print("page: ${controller.page}, index: $index");
+          zoom = controller.page - index;
+        } else {
+          zoom = currentpage*1.0 - index;
+        }
+        zoom = (1 - zoom.abs() * .3).clamp(0.0, 1.0);
+        var scale = Curves.easeOut.transform(zoom),
+            height = scale * (constraints.maxHeight - 260),
+            width = scale * (constraints.maxWidth*0.4-10);
+        var log = """index: $index,
+            value: $zoom,
+            transform: ${scale.toStringAsFixed(2)},
+            height: ${height.toStringAsFixed(2)}
+            width: ${width.toStringAsFixed(2)}"""
+            .replaceAll("\n", "")
+            .replaceAll(" ", "");
+        print(log);
+        return Center(
+          child: SizedBox(
+              width: width,
+              height: height,
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 160.0),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10.0),
+                    color: Colors.green[p],
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 7.0,
+                          offset: Offset(2.0, 2.0)),
+                    ]),
+                child: Center(
+                    child: Text(
+                  "$index\n${scale.toStringAsFixed(2)}",
+                  style: TextStyle(fontSize: 50.0, color: Colors.white),
+                )),
+              )),
+        );
+      },
     );
-    return SafeArea(
-        bottom: true,
-        child: AnimatedBuilder(
-            animation: controller,
-            builder: (context, child) {
-              var zoom = 1.0;
-              if (controller.position.haveDimensions) {
-                // print("page: ${controller.page}, index: $index");
-                zoom = controller.page - index;
-                zoom = (1 - zoom.abs() * .5).clamp(0.0, 1.0);
-              }
-              print("transform: ${Curves.easeOut.transform(zoom)}, $constraints");
-              return SizedBox(
-                width: Curves.easeOut.transform(zoom) * 300,// (constraints.maxWidth-20),
-                height: Curves.easeOut.transform(zoom) * 200,//(constraints.maxHeight-100),
-                child: item,
-              );
-            },
-            child: item));
+    // child: item);
   }
 }
